dfebf0e62f38beeffe4aa1642cc490c6f1fc9702
rocky1638 <rockzhou15@gmail.com>
2018-01-26 16:03:09 -0500

    added log createdAt middleware

--- models/Log.js
+++ models/Log.js
@@ -4,10 +4,17 @@ const Schema = mongoose.Schema;
 const LogSchema = new Schema(
   {
     title: String,
-    date: Date,
+    createdAt: Date,
     description: String
   },
   { usePushEach: true }
 );
+
+LogSchema.pre('save', function preSave(next) {
+  var log = this;
+
+  log.createdAt(Date.now());
+  next();
+});
 
 mongoose.model('log', LogSchema);
